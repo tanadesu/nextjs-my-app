28c67414fe4a60cc8cdc1363e4f7351eeca5922b
田中奏佑 <tanakakanadeyuu@tanakakanadeyuunoMacBook-Pro.local>
2026-05-08 09:59:05 +0900

    Configure Supabase project

--- app/supabase-config.js
+++ app/supabase-config.js
@@ -1,4 +1,4 @@
 window.SUPABASE_CONFIG = {
-  url: "",
-  anonKey: "",
+  url: "https://iujqszgqumxogwggqaxk.supabase.co",
+  anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml1anFzemdxdW14b2d3Z2dxYXhrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzgxNzkzMTEsImV4cCI6MjA5Mzc1NTMxMX0.YJKxAu2WmZsQInOL3Y8d4lUXQ09NDUEBAr6XWWZ8-Co",
 };
